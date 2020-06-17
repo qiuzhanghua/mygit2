d774f19b86247983f93d242e8139c4981f0cddd1
Daniel <qiuzhanghua@icloud.com>
2020-06-17 21:30:45 +0800

    clone & open repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn open_repo() {
+        use git2::Repository;
+
+        let repo = match Repository::open(".") {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to open: {}", e),
+        };
+
+        assert!(repo.find_remote("origin").is_ok());
+    }
+
+    #[test]
+    fn test_clone_with_ssh() {
+        use git2::{Cred, Error, RemoteCallbacks};
+        use std::env;
+        use std::path::Path;
+
+        // Prepare callbacks.
+        let mut callbacks = RemoteCallbacks::new();
+        callbacks.credentials(|_url, username_from_url, _allowed_types| {
+            Cred::ssh_key(
+                username_from_url.unwrap(),
+                None,
+                std::path::Path::new(&format!("{}/.ssh/id_rsa", env::var("HOME").unwrap())),
+                None,
+            )
+        });
+
+        // Prepare fetch options.
+        let mut fo = git2::FetchOptions::new();
+        fo.remote_callbacks(callbacks);
+
+        // Prepare builder.
+        let mut builder = git2::build::RepoBuilder::new();
+        builder.fetch_options(fo);
+
+        // Clone the project.
+        assert!(builder
+            .clone(
+                "git@github.com:qiuzhanghua/mygit2.git",
+                Path::new("/tmp/mygit2"),
+            )
+            .is_ok());
     }
 }
